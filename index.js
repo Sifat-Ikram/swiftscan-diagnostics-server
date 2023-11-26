c4a7940d4cbad6343a0ac6a73fc767c073dfc89a
Sifat Ikram <sifatikram@gmail.com>
2023-11-26 19:55:55 +0600

    jwt installed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 4321;
 
 
@@ -9,6 +10,34 @@ const port = process.env.PORT || 4321;
 
 app.use(cors());
 app.use(express.json());
+
+
+const verifyToken = (req, res, next) => {
+  console.log(req.headers);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'Access forbidden' })
+  }
+  next();
+}
 
 
 
@@ -34,15 +63,60 @@ async function run() {
     const userCollection = client.db("swiftscanDB").collection("user");
 
 
+    // jwt api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
     // user api
     app.post('/user', async (req, res) => {
       const user = req.body;
-      // const query = { email: user.email };
-      // const ifExist = await userCollection.findOne(query);
-      // if (ifExist) {
-      //   return res.send({ message: 'user already exist', insertedId: null })
-      // }
+      const query = { email: user.email };
+      const ifExist = await userCollection.findOne(query);
+      if (ifExist) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.get('/user', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/user/admin/:email', verifyToken,  async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/user/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
